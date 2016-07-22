5e2b287d484c768fa0ad15dbec5eba1371decc9a
Gogo Lejzr <gogo.lejzr@gmail.com>
2016-07-22 22:10:23 +0200

    more cleanup, formating

--- ArduinoRF24_I2C_DHT11/ArduinoRF24_I2C_DHT11.ino
+++ ArduinoRF24_I2C_DHT11/ArduinoRF24_I2C_DHT11.ino
@@ -1,8 +1,8 @@
 /*
-LCD i2c library: https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
-LCD i2c module: for example http://www.aliexpress.com/item/LCD1602-I2C-LCD-1602-module-Blue-screen-IIC-I2C-for-arduino-LCD1602-Adapter-plate/32651314933.html
-DHT11 library: https://github.com/RobTillaart/Arduino/tree/master/libraries/DHTlib
-*/
+   LCD i2c library: https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
+   LCD i2c module: for example http://www.aliexpress.com/item/LCD1602-I2C-LCD-1602-module-Blue-screen-IIC-I2C-for-arduino-LCD1602-Adapter-plate/32651314933.html
+   DHT11 library: https://github.com/RobTillaart/Arduino/tree/master/libraries/DHTlib
+ */
 
 #include <dht.h>
 #include <Wire.h>
@@ -19,46 +19,46 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  pinMode(DHT11_POWER_PIN, OUTPUT);
-  digitalWrite(DHT11_POWER_PIN, HIGH);
+    pinMode(DHT11_POWER_PIN, OUTPUT);
+    digitalWrite(DHT11_POWER_PIN, HIGH);
 
-  lcd.begin();
-  lcd.backlight();
+    lcd.begin();
+    lcd.backlight();
 }
 
 void loop()
 {
-  // READ DATA
-  int chk = DHT.read11(DHT11_PIN);
-  switch (chk)
-  {
-    case DHTLIB_OK:  
-		Serial.print("OK,\t"); 
-		break;
-    case DHTLIB_ERROR_CHECKSUM: 
-		Serial.print("Checksum error,\t"); 
-		break;
-    case DHTLIB_ERROR_TIMEOUT: 
-		Serial.print("Time out error,\t"); 
-		break;
-    default: 
-		Serial.print("Unknown error,\t"); 
-		break;
-  }
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(TEMP_STR);
-  lcd.setCursor(6,0);
-  lcd.print(DHT.temperature);
-  lcd.setCursor(0,1);
-  lcd.print(HUM_STR);
-  lcd.setCursor(6,1);
-  lcd.print(DHT.humidity);
+    int chk = DHT.read11(DHT11_PIN);
+    switch (chk)
+    {
+        case DHTLIB_OK:
+            Serial.print("OK,\t");
+            break;
+        case DHTLIB_ERROR_CHECKSUM:
+            Serial.print("Checksum error,\t");
+            break;
+        case DHTLIB_ERROR_TIMEOUT:
+            Serial.print("Time out error,\t");
+            break;
+        default:
+            Serial.print("Unknown error,\t");
+            break;
+    }
 
-  delay(2000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(TEMP_STR);
+
+    lcd.setCursor(6,0);
+    lcd.print(DHT.temperature);
+
+    lcd.setCursor(0,1);
+    lcd.print(HUM_STR);
+
+    lcd.setCursor(6,1);
+    lcd.print(DHT.humidity);
+
+    delay(2000);
 }
-//
-// END OF FILE
-//
